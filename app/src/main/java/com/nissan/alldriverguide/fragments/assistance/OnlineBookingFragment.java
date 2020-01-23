8d58eefbe71c368e594eb204161d7b3d842b964b
Rohan Talukder <ahmed.talukder@datasoft-bd.com>
2020-01-23 12:42:32 +0600

    Url For Telephone

--- app/src/main/java/com/nissan/alldriverguide/fragments/assistance/OnlineBookingFragment.java
+++ app/src/main/java/com/nissan/alldriverguide/fragments/assistance/OnlineBookingFragment.java
@@ -1,26 +1,31 @@
 package com.nissan.alldriverguide.fragments.assistance;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.nissan.alldriverguide.MainActivity;
 import com.nissan.alldriverguide.R;
-import com.nissan.alldriverguide.utils.Logger;
 
 public class OnlineBookingFragment extends Fragment {
 
     private WebView webView;
     private LinearLayout linearBack;
+    private ImageButton btnBack;
     private TextView txtViewTitle;
     private static final String TITLE = "title";
     private static final String IMG_URL = "img_url";
@@ -51,6 +56,12 @@ public class OnlineBookingFragment extends Fragment {
 
         txtViewTitle.setText(getArguments().getString(TITLE));
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.clearCache(true);
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new MyWebViewClient());
 
         webView.loadUrl(getArguments().getString(IMG_URL));
@@ -62,6 +73,13 @@ public class OnlineBookingFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 ((MainActivity) getActivity()).onBackPressed();
+
+            }
+        });
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) getActivity()).onBackPressed();
             }
         });
     }
@@ -69,6 +87,7 @@ public class OnlineBookingFragment extends Fragment {
     private void initView() {
         webView = (WebView) view.findViewById(R.id.webView_online_booking);
         linearBack = (LinearLayout) view.findViewById(R.id.linear_back);
+        btnBack = (ImageButton) view.findViewById(R.id.btn_back);
         txtViewTitle = (TextView) view.findViewById(R.id.txt_title);
         progressBar = (ProgressBar) view.findViewById(R.id.prog_online);
     }
@@ -76,8 +95,15 @@ public class OnlineBookingFragment extends Fragment {
     private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
+            /*view.loadUrl(url);
+            return true;*/
+
+            if (url.contains("tel:")) {
+                startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(url)));
+                return true;
+            } else {
+                return false;
+            }
         }
 
         @Override
